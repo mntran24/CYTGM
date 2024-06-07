e5a492c08fdd54c5fa4e31cfa7d6b6d663b1c00e
student <student@admins-MacBook-Pro.local>
2024-06-07 17:42:52 -0400

    implemented json and decoder

---
+++ CYTGM/CYTGM/Tag.swift
@@ -0,0 +1,13 @@
+//
+//  Tag.swift
+//  CYTGM
+//
+//  Created by student on 6/7/24.
+//
+
+import Foundation
+import SwiftData
+struct Tag: Codable{
+    var name: String
+    var description: String
+}
